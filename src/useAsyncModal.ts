926ed714f1a92a5a3503e0cfc875e36c7c3c8b4a
Stephan Deininghaus <stephan@deininghaus.com>
2023-06-29 21:41:02 +0200

    Add TSDoc comments for public API elements

--- src/useAsyncModal.ts
+++ src/useAsyncModal.ts
@@ -1,5 +1,14 @@
 import { AsyncModalProps, useAsyncModalState } from './useAsyncModalState'
 
+/**
+ * Encapsulates state for a modal UI element and yields a rendered React
+ * element when the modal is open. The `props` parameter passed to the render
+ * function includes `resolve` and `reject` callbacks that can be used to
+ * conclude the interaction.
+ *
+ * @param renderModal - Function that renders modal UI from given `props`
+ * @returns Array with `modal` and `showModal` elements
+ */
 export function useAsyncModal<TValue = void, TResult = void>(
   renderModal: (props: AsyncModalProps<TValue, TResult>) => React.ReactNode,
 ): [
--- src/useAsyncModalState.ts
+++ src/useAsyncModalState.ts
@@ -1,15 +1,35 @@
 import { useCallback, useState } from 'react'
 
+/**
+ * Provides invocation arguments and callbacks to resolve/reject the interaction
+ * to a modal UI element.
+ */
 export interface AsyncModalProps<TValue, TResult> {
   value: TValue
   resolve: (result: TResult | PromiseLike<TResult>) => void
   reject: (reason?: any) => void
 }
 
+/**
+ * Interaction state for a modal UI element as managed by `useAsyncModalState`.
+ * Indicates whether the modal is currently open and contains invocation arguments
+ * and callbacks to resolve/reject the interaction.
+ */
 export type AsyncModalState<TValue, TResult> =
   | { isOpen: true; props: AsyncModalProps<TValue, TResult> }
   | { isOpen: false; props: null }
 
+/**
+ * Manages the state of a modal UI element that can be invoked asynchronously
+ * using the returned `showModal` callback.
+ *
+ * @remarks
+ * The `TValue` and `TResult` type parameters indicate the type of the argument to
+ * `showModal` and the type of its return value, respectively. Both default to `void`,
+ * i.e., a modal that takes no arguments and returns no value upon completion.
+ *
+ * @returns Array with `state` and `showModal` elements
+ */
 export function useAsyncModalState<TValue = void, TResult = void>(): [
   state: AsyncModalState<TValue, TResult>,
   showModal: (value: TValue) => Promise<TResult>,
